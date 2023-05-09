2f9f2719eceda4ebfad707f0845d122113c5e00e
Amit Pandey <amit81601@gmail.com>
2023-05-09 23:40:47 +0530

    message

---
+++ hr.java
@@ -0,0 +1,22 @@
+class main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        /* Variable
+        Just Like:
+        Water - Bucket
+        Masala - box
+        Lunch - LunchBox
+        In Java:
+        Variables are containers which store data values
+        String, int, float, char, boolean
+        How do you declare variables:
+        Syntex - <dataType> <variableName> = <value>;
+        */ 
+        String name = "Amit";
+        System.out.println(name);
+        
+        int a = 24;
+        float b = 45.22f;
+        System.out.println(a);
+    }
+}
