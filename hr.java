b35174e8911d6d41e81bb1a9da40a6641c6b127e
Amit Pandey <amit81601@gmail.com>
2023-05-13 15:41:24 +0530

    massage

--- hr.java
+++ hr.java
@@ -1,10 +1,10 @@
-import javax.naming.spi.DirStateFactory.Result;
-import javax.print.attribute.standard.Compression;
+import java.util.Scanner;
 
 class main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        /* Variable
+
+        public static void main(String[] args) { 
+       /*  System.out.println("Hello, World!");
+         Variable
         Just Like:
         Water - Bucket
         Masala - box
@@ -16,8 +16,19 @@ class main {
         Syntex - <dataType> <variableName> = <value>;
         */ 
         String name = "Amit";
-        System.out.println(name);
-        
+        String channel = "CodeWithAmit";
+       System.out.println(name);
+        System.out.println(name.length());
+        System.out.println(name.toUpperCase());
+        System.out.println(name + " from\" " + channel);
+        System.out.println(name + " from\\ " + channel);
+        System.out.println(name + " from\t " + channel);
+        System.out.println(name + " from\n " + channel); 
+        System.out.println(name.contains("Am"));
+        System.out.println(name.charAt(2)); 
+        System.out.println(name.endsWith("it")); 
+        System.out.println(name.indexOf("mit"));      
+         /* 
         int a = 24;
         float b = 45.22f;
         boolean isAdult = false;
@@ -39,6 +50,7 @@ class main {
               duble (8 bytes), boolean(1 bit), char (2 byte)
             2.Non primitive or reference data type - 
              */
+            /* 
             byte u = 56;
             double d = 45.645435345d;
             System.out.println(u);
@@ -46,6 +58,7 @@ class main {
             char grade = 'A';
             System.out.println(grade);
               
+            */
             /*operators in java
 
             operand, opretor, operand = Result
@@ -58,7 +71,11 @@ class main {
             2. Assignment operators
             4. Compression operatoCompression
             */ 
-            int num1 = 45, num2=78;
+            /*int num1 = 45,  num2=78;
+            num1 += 3;
+            num2 -= 8; 
+            /* Expoler these opretors - *=, /=, %= */
+            /* 
             System.out.print("The value of num1 + num2 is ");
             System.out.println(num1 + num2);
 
@@ -73,10 +90,30 @@ class main {
 
             System.out.print("The value of num1 % num2 is ");
             System.out.println(num1 % num2); 
-             System.out.println(num1++);
+             System.out.println(num2++);
              System.out.println(++num1);
              System.out.println(num1--);
-             System.out.println(--num1); 
+             System.out.println(--num1);
+               */
+             /* Comparison Operstors
+              * 1. == check for equality of two values
+              * 2. != : check if two values are not equal
+              * 3. <
+              * 4. >
+              * 5. <=
+              * 6. >=
+
+              * Logical operators:
+              * 1. && - Logical and operator - returns true only if both condition are true
+              * 2. || - Logical and operator - returns true if any one condition is true 
+              * 3. ! - Logical not - Reverse the result frpm true to false and vice versa
+              */
+              // Testing  user input in java 
+             /*  Scanner scan = new Scanner(System.in);
+              System.out.println("Enter Age");
+              int input = scan.nextInt();
+              System.out.println(input);
+              */
 
         } 
     }
