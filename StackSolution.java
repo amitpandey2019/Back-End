8318c3166df34d37966da5ebeb48f9d1009dbfc2
Amit Pandey <amit81601@gmail.com>
2023-06-19 22:47:43 +0530

    massage

--- StackSolution.java
+++ StackSolution.java
@@ -1,38 +1,30 @@
+
+
 import java.util.Stack;
-
-import javax.swing.text.html.HTMLDocument.Iterator;
-
 public class StackSolution {
-    public static void main(String[]args){
-        // creacted a stack
-    Stack<Integer> s1=new Stack<>();
-    // inserting element into stack
-    s1.push(2);
-    s1.push(4);
-    s1.push(9);
-    System.out.println(s1);
-// the delete opration in stack
-    //s1.pop();
-    System.out.println(s1);
-    //peek opration
-    System.out.println(s1.peek());
-    //s1.pop();
-    //s1.pop();
-    boolean status = s1.empty(); 
-    System.out.println(status);
-    int indexfound = s1.search(4);
-    System.out.println(indexfound);
-//how to iterate over a stack
-//Iterator it = s1.iterator();
-//while(it.hasNext()){
- //      Object s2 = it.next();
-  //System.out.print(s2);
-    }
-    System.out.println();
-    System.out.println(s1.size());
-    s1.sort(null);
-    System.out.println(s1);
+    public static void main(String[]args) {
+      Stack<Integer> s1=new Stack<>();
+//inserting element into stack
+      s1.push(2);
+      s1.push(4);
+      s1.push(9);
+      System.out.println(s1);
+// delete opration on stack
+      s1.pop(); 
+      System.out.println(s1);
+     // peek opration 
+      System.out.println(s1.peek());
+      s1.pop();
+      s1.pop();
+      boolean status = s1.empty();
+      System.out.println(status);
+      int indexfound = s1.search(4);
+      System.out.println(indexfound);
 
-        
-    }    
-        
+      Iterator it = s1.iterator();
+     while(it.hasNext()){
+      Obeject s2 = it.next();
+      System.out.print(s2);
+     }
+    }
+}
